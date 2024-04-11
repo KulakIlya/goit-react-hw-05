667db139725bf07055edbd74d014764aacfcf009
Ilya <kulak1224@gmail.com>
2024-04-11 13:40:34 +0300

    adding pages and components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,22 @@
+import { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const HomePage = lazy(() => import('../../pages/HomePage'));
+const MovieDetailsPage = lazy(() => import('../../pages/MovieDetailsPage'));
+const MoviesPage = lazy(() => import('../../pages/MoviesPage'));
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />}>
+            <Route path=":id" element={<MovieDetailsPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 export default App;
